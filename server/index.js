b1c536b8754fea3a027730cea214bfae202f5801
Tommy <tsuwunrut@carecloud.com>
2019-02-23 01:34:17 -0500

    express clean up

--- server/index.js
+++ server/index.js
@@ -5,17 +5,13 @@ const axios = require("axios");
 //Create a new express app by calling the express function and store it in a constant named app
 const app = express();
 
-//Instruct express to use express.static middleware
-//in order to serve static assets located at ./client/build
-app.use(express.static(__dirname + "/client/build"));
-
 app.get("/home", (req, res) => {
   console.log("hello world");
 });
 
 //Create new request handler that when receiving a
-//GET request to the /animals endpoint
-//will send back a collection of objects
+//GET request to the /characters endpoint
+//will send back a JSON collection of characters
 app.get("/characters", async (req, res) => {
   const { data } = await axios.get("https://swapi.co/api/people");
   return res.send(data);
